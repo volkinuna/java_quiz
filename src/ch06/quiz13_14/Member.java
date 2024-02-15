08e0fdf2b4ff8a42a0ce5434955f9ecfb2121af0
volkinuna <plleiades@gmail.com>
2024-02-15 17:50:50 +0900

    클래스 13~14번 퀴즈

---
+++ src/ch06/quiz13_14/Member.java
@@ -0,0 +1,16 @@
+package ch06.quiz13_14;
+
+public class Member {
+
+    // 필드 (13번)
+    String name;
+    String id;
+    String password;
+    int age;
+
+    // 생성자 (14번)
+    Member(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
