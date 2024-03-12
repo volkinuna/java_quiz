69357cbaa96a53b0f180b05363b9961502389d00
volkinuna <plleiades@gmail.com>
2024-03-12 17:55:49 +0900

    컬렉션 자료구조 퀴즈

--- src/ch15/quiz8/Student.java
+++ src/ch15/quiz8/Student.java
@@ -3,20 +3,27 @@ package ch15.quiz8;
 public class Student {
     public int studentNum;
     public String name;
-    public Student (int studentNum, String name) {
+
+    public Student(int studentNum, String name) {
         this.studentNum = studentNum;
         this.name = name;
     }
+
     //여기에 코드를 작성하세요.
     @Override
     public int hashCode() {
-        return name.hashCode() + studentNum;
+        return studentNum;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (obj instanceof Student target) {
-            return target.name.equals(name) && (target.studentNum == studentNum);
+        if (obj instanceof Student) {
+            Student student = (Student) obj;
+            if (student.studentNum == studentNum) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
